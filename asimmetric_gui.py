8cddef0b4ff16286a5ebc428ad8cb3a21020141d
pakshaA <pakshaartem@gmail.com>
2022-05-06 02:18:40 +0300

    adapted to start from main window

--- asimmetric_gui.py
+++ asimmetric_gui.py
@@ -6,59 +6,55 @@ from key_gen import *
 import os
 
 
-class Ui_MainWindow(object):
+class Ui_RSA(object):
     fname = ""
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(516, 552)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.Select_dir = QtWidgets.QPushButton(self.centralwidget)
+    def setupUi(self, RSA):
+        RSA.setObjectName("RSA")
+        RSA.resize(516, 552)
+        self.Select_dir = QtWidgets.QPushButton(RSA)
         self.Select_dir.setGeometry(QtCore.QRect(240, 140, 251, 31))
         self.Select_dir.setObjectName("Select_dir")
-        self.Method = QtWidgets.QComboBox(self.centralwidget)
+        self.Method = QtWidgets.QComboBox(RSA)
         self.Method.setGeometry(QtCore.QRect(39, 20, 101, 22))
         self.Method.setObjectName("Method")
         self.Method.addItem("")
         self.Method.addItem("")
-        self.Open_dir_to_save = QtWidgets.QPushButton(self.centralwidget)
+        self.Open_dir_to_save = QtWidgets.QPushButton(RSA)
         self.Open_dir_to_save.setGeometry(QtCore.QRect(290, 300, 111, 23))
         self.Open_dir_to_save.setObjectName("Open_dir_to_save")
-        self.Select_file_encr = QtWidgets.QLabel(self.centralwidget)
+        self.Select_file_encr = QtWidgets.QLabel(RSA)
         self.Select_file_encr.setGeometry(QtCore.QRect(40, 110, 191, 71))
         self.Select_file_encr.setObjectName("select_file")
-        self.Select_file_decr = QtWidgets.QLabel(self.centralwidget)
+        self.Select_file_decr = QtWidgets.QLabel(RSA)
         self.Select_file_decr.setGeometry(QtCore.QRect(40, 110, 191, 71))
         self.Select_file_decr.setObjectName("Select_file")
-        self.Keys_gen = QtWidgets.QPushButton(self.centralwidget)
+        self.Keys_gen = QtWidgets.QPushButton(RSA)
         self.Keys_gen.setGeometry(QtCore.QRect(160, 20, 281, 21))
         self.Keys_gen.setObjectName("Keys_gen")
-        self.Full_Start = QtWidgets.QPushButton(self.centralwidget)
+        self.Full_Start = QtWidgets.QPushButton(RSA)
         self.Full_Start.setGeometry(QtCore.QRect(40, 70, 451, 23))
         self.Full_Start.setObjectName("Full_Start")
-        self.End = QtWidgets.QPushButton(self.centralwidget)
+        self.End = QtWidgets.QPushButton(RSA)
         self.End.setGeometry((QtCore.QRect(44, 370, 451, 23)))
         self.End.setObjectName("End")
-        self.dir_to_keys = QtWidgets.QLabel(self.centralwidget)
+        self.dir_to_keys = QtWidgets.QLabel(RSA)
         self.dir_to_keys.setGeometry(QtCore.QRect(90, 50, 411, 71))
         self.dir_to_keys.setObjectName("dir_to_keys")
-        self.Save_text_encr = QtWidgets.QLabel(self.centralwidget)
+        self.Save_text_encr = QtWidgets.QLabel(RSA)
         self.Save_text_encr.setGeometry(QtCore.QRect(40, 220, 451, 61))
         self.Save_text_encr.setObjectName("Save_text")
-        self.Save_text_decr = QtWidgets.QLabel(self.centralwidget)
+        self.Save_text_decr = QtWidgets.QLabel(RSA)
         self.Save_text_decr.setGeometry(QtCore.QRect(40, 220, 451, 61))
         self.Save_text_decr.setObjectName("Save_text")
-        self.Tip_to_open = QtWidgets.QLabel(self.centralwidget)
+        self.Tip_to_open = QtWidgets.QLabel(RSA)
         self.Tip_to_open.setGeometry(QtCore.QRect(40, 280, 241, 61))
         self.Tip_to_open.setObjectName("Tip_to_open")
-        self.Start = QtWidgets.QPushButton(self.centralwidget)
+        self.Start = QtWidgets.QPushButton(RSA)
         self.Start.setGeometry(QtCore.QRect(70, 180, 121, 23))
         self.Start.setObjectName("Start")
-        MainWindow.setCentralWidget(self.centralwidget)
 
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.retranslateUi(RSA)
+        QtCore.QMetaObject.connectSlotsByName(RSA)
 
         self.Start.clicked.connect(self.start_click)
         self.Keys_gen.clicked.connect(self.keys_gen)
@@ -83,7 +79,7 @@ class Ui_MainWindow(object):
 
         self.Keys_gen.setText(_translate("MainWindow", "Нажмите, чтобы сгенерировать пару ключей"))
         self.dir_to_keys.setText(_translate("MainWindow",
-                                            "<html><head/><body><p>Ключи сгенерированы в C:\\Users\\paksh\\PycharmProjects\\encryptor\\private</p><p>и C:\\Users\\paksh\\PycharmProjects\\encrypto\\public</p></body></html>"))
+                                            "<html><head/><body><p>Ключи сгенерированы в C:\\Users\\paksh\\PycharmProjects\\encryptor\\private</p><p>и C:\\Users\\paksh\\PycharmProjects\\encryptor\\public</p></body></html>"))
 
         self.Save_text_encr.setText(_translate("MainWindow", "Файл с зашифрованным текстом сохранен в\n"
                                                         " C:Users\\paksh\\PycharmProjects\\test_with_text\\data_encrypted"))
@@ -160,14 +156,3 @@ class Ui_MainWindow(object):
             os.system("C:\\Users\\paksh\\PycharmProjects\\encryptor\\data_encrypted.txt")
         if self.Method.currentIndex() == 1:
             os.system("C:\\Users\\paksh\\PycharmProjects\\encryptor\\data_encrypted_decrypted.txt")
-
-
-if __name__ == "__main__":
-    import sys
-
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
